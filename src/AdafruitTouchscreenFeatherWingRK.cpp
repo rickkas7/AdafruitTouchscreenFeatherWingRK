97ad3aa178311c1010572164176204210cd84888
rickkas7 <rickk@rickk.com>
2018-12-02 11:23:40 -0500

    Version 0.0.1

---
+++ src/AdafruitTouchscreenFeatherWingRK.cpp
@@ -0,0 +1,1 @@
+#include "AdafruitTouchscreenFeatherWingRK.h"
---
+++ src/AdafruitTouchscreenFeatherWingRK.h
@@ -0,0 +1,9 @@
+#ifndef __ADAFRUITTOUCHSCREENFEATHERWINGRK_H
+#define __ADAFRUITTOUCHSCREENFEATHERWINGRK_H
+
+#include "Adafruit_ILI9341_RK.h"
+#include "Adafruit_STMPE610_RK.h"
+
+// TODO: Add SdFat support when library supports mesh
+
+#endif /* __ADAFRUITTOUCHSCREENFEATHERWINGRK_H */
